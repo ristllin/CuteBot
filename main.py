d82642912b8bb6713d751c942639e4e5a6202d45
Roy Darnell <ristlin@gmail.com>
2021-07-22 12:16:42 +0300

    identify specific

--- main.py
+++ main.py
@@ -20,7 +20,23 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
+#person identifier
+known_image = face_recognition.load_image_file("Roy.jpg")
+target_encoding = face_recognition.face_encodings(known_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    target_encoding,
+]
+known_face_names = [
+    "Roy",
+]
+
+
 while True:
+    face_locations = []
+    face_encodings = []
+    face_names = []
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
@@ -35,6 +51,18 @@ while True:
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        face_names.append(name)
 
     # Display the results
     for top, right, bottom, left in face_locations:
@@ -46,6 +74,10 @@ while True:
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
 
 
